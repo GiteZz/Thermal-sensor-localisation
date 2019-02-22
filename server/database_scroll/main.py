d87a185071d4d7639fe2370deb7bf7a5d4a8aae5
gballege <gilles.ballegeer@ugent.be>
2019-02-22 10:01:17 +0100

    Basis in place

--- server/database_scroll/main.py
+++ server/database_scroll/main.py
@@ -1,12 +1,16 @@
 import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtGui import QIcon, QPixmap, QImage
+import scipy.ndimage.filters as fil
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import numpy as np
 
 from ui_generated import Ui_MainWindow
 from db_model import Measurement, Base
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
 
 class MyUI(QtWidgets.QMainWindow):
     def __init__(self):
@@ -18,6 +22,8 @@ class MyUI(QtWidgets.QMainWindow):
         self.session = self.Session()
         self.episode_index = 0
         self.time_index = 0
+        self.more_jump = 4
+
 
     def confirmUI(self, ui_widgets):
         print("confirming ui")
@@ -26,11 +32,44 @@ class MyUI(QtWidgets.QMainWindow):
         self.widgets.sensorList.currentRowChanged.connect(self.sensor_clicked)
         self.widgets.timeList.currentRowChanged.connect(self.episode_clicked)
 
+        self.widgets.forwardOneButton.clicked.connect(self.one_forward)
+        self.widgets.backwardOneButton.clicked.connect(self.one_backward)
+        self.widgets.forwardMoreButton.clicked.connect(self.more_forward)
+        self.widgets.backwardMoreButton.clicked.connect(self.more_backward)
+
+        self.widgets.timeSlider.valueChanged.connect(self.move_timeslider)
+
+        self.figure = Figure()
+        self.canvas = FigureCanvas(self.figure)
+        self.widgets.rightVLayout.insertWidget(0,self.canvas)
+        self.ax0 = self.figure.add_subplot(1,2,1)
+        self.ax1 = self.figure.add_subplot(1,2,2)
+
+    def one_forward(self):
+        if self.time_index < len(self.list_episodes[self.episode_index]) - 1:
+            self.widgets.timeSlider.setValue(self.time_index + 1)
+
+    def one_backward(self):
+        if self.time_index >= 1:
+            self.widgets.timeSlider.setValue(self.time_index - 1)
+
+    def more_forward(self):
+        if self.time_index < len(self.list_episodes[self.episode_index]) - 1 - self.more_jump:
+            self.widgets.timeSlider.setValue(self.time_index + self.more_jump)
+
+    def more_backward(self):
+        if self.time_index >= self.more_jump:
+            self.widgets.timeSlider.setValue(self.time_index + self.more_jump)
+
+    def move_timeslider(self, value):
+        self.time_index = value
+        self.draw_plot()
+
     def sensor_clicked(self, index):
         print('clicked!  => ' + str(index))
         sensor = int(self.widgets.sensorList.item(index).text())
         sensor_values = self.session.query(Measurement).filter(Measurement.sensor_id == sensor). \
-            order_by(Measurement.timestamp.desc()).limit(100).all()
+            order_by(Measurement.timestamp.desc()).limit(1000).all()
 
         self.list_episodes = []
         current_starttime = sensor_values[0].timestamp
@@ -40,11 +79,12 @@ class MyUI(QtWidgets.QMainWindow):
             if diff < 60:
                 current_episode.append(value)
             else:
-                self.list_episodes.append(current_episode)
+                self.list_episodes.append(current_episode[::-1])
                 current_episode = []
             current_starttime = value.timestamp
+
         if len(current_episode) > 0:
-            self.list_episodes.append(current_episode)
+            self.list_episodes.append(current_episode[::-1])
 
         for episode in self.list_episodes:
             self.widgets.timeList.addItem(str(episode[0].timestamp).split('.')[0])
@@ -54,25 +94,26 @@ class MyUI(QtWidgets.QMainWindow):
 
     def episode_clicked(self, index):
         self.episode_index = index
-        img_ar = np.array(self.list_episodes[self.episode_index][self.time_index].data).reshape((32,24))
-        max_img = np.max(img_ar)
-        min_img = np.min(img_ar)
-        min_diff = max_img - min_img
+        self.widgets.timeSlider.setMinimum(0)
+        self.widgets.timeSlider.setMaximum(len(self.list_episodes[self.episode_index]) - 1)
+        self.draw_plot()
 
-        img_ar = ((img_ar - min_img)/min_diff) * 255
-        rgb_img = np.zeros((32,24,3))
-        rgb_img[:,:,0] = img_ar
-        rgb_img[:, :, 1] = np.zeros((32,24))
-        rgb_img[:, :, 2] = 255 - img_ar
 
-        rgb_img = np.repeat(rgb_img, 20, axis=0)
-        rgb_img = np.repeat(rgb_img, 20, axis=1)
 
-        height, width, channel = rgb_img.shape
-        bytesPerLine = 3 * width
-        qImg = QImage(rgb_img.data, width, height, bytesPerLine, QImage.Format_RGB888).rgbSwapped()
-        pix = QPixmap.fromImage(qImg)
-        self.widgets.imageLabel.setPixmap(pix)
+    def draw_plot(self):
+        img_ar = np.transpose(np.array(self.list_episodes[self.episode_index][self.time_index].data).reshape((32,24)))
+        result = fil.gaussian_filter(img_ar, 1)
+
+        c = self.ax0.pcolor(img_ar)
+        # self.ax0.colorbar(c, ax=self.ax0)
+        # plt.gca().set_aspect('equal', adjustable='box')
+        d = self.ax1.pcolor(result)
+        # self.ax1.colorbar(c, ax=self.ax1)
+        # plt.gca().set_aspect('equal', adjustable='box')
+        self.ax1.axis('equal')
+        self.ax0.axis('equal')
+
+        self.canvas.draw()
 
     def get_data_db(self):
         sensor_ids = self.session.query(Measurement).distinct(Measurement.sensor_id).all()
