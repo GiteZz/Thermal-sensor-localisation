13cf94a0f9cdc383a70a51fb18d4ff0c1b1a9b70
tlpss <thomas17.lips@gmail.com>
2019-03-28 14:46:22 +0100

    implemented calibrate and update function of localizer

--- server/tests/Localiser_test.py
+++ server/tests/Localiser_test.py
@@ -1,22 +1,34 @@
-from help_module.OLD_localisation_helper import Localiser
+from localization.localiser import Localiser
 
-#scenario 1 (hoek=0)
-center=[500,200]
-corner=[336.1696,85.2847]
-l=Localiser();
-l.set_heigt(380)
-l.set_corner_and_center(corner,center)
-l.centroids=[[0,0],[300,200]]
-ret=l.get_abs_locations()
-print(ret)
-print("------------")
-#scenario 2
-center=[500,200]
-corner=[318.52,115.2254]
-l=Localiser();
-l.set_heigt(380)
-l.set_corner_and_center(corner,center)
-print('error:'+str(l.get_error()))
-l.centroids=[[0,0],[300,200]]
-ret=l.get_abs_locations()
-print(ret)
+H=Localiser()
+#points 5,7,8,9,10,11
+H.add_calibration_point(143,74,210,304)
+H.add_calibration_point(265,29,106,32)
+H.add_calibration_point(264,115,287,83)
+H.add_calibration_point(126,164,345,337)
+H.add_calibration_point(112,31,128,342)
+H.add_calibration_point(44,124,272,481)
+m=H.determine_matrix()
+cord=H.get_world_coords(44,124)
+cord/=cord[2]
+print(str(cord) +'vs' +'(272,481)')
+
+cord = H.get_world_coords(264, 115)
+cord /= cord[2]
+print(str(cord) + 'vs' + '(287,83)')
+
+cord = H.get_world_coords(265, 29)
+cord /= cord[2]
+print(str(cord) + 'vs' + '(106,32)')
+
+cord = H.get_world_coords(143, 74)
+cord /= cord[2]
+print(str(cord) + 'vs' + '(210,304)')
+cord = H.get_world_coords(126, 164)
+cord /= cord[2]
+print(str(cord) + 'vs' + '(345,337)')
+# new points
+cord = H.get_world_coords(72, 75)
+cord /= cord[2]
+print(str(cord) + 'vs' + '(199,415)')
+
